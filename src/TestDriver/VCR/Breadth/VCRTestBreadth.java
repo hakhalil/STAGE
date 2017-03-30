17c8b0e630117ab63e8f5e1f95c9e075d1f22078
amrsellaithy <amr.ellaithy@gmail.com>
2017-03-29 23:51:23 -0400

    adding vcr breadth

---
+++ src/TestDriver/VCR/Breadth/VCRTestBreadth.java
@@ -0,0 +1,30 @@
+package TestDriver.VCR.Breadth;
+import org.junit.Test;
+
+import TestDriver.OverflowException;
+import TestDriver.TestDriver;
+import junit.framework.TestCase;
+
+
+public class ReplaceThis extends TestCase{
+
+	int preLast = 0;
+	boolean result = false;
+	int[] preElements = {};
+	int resulting = 0;
+	@Test
+	public void test1() throws OverflowException {
+		
+		TestDriver driver = new TestDriver();
+		 
+//		driver.readStateMap("StateMap.txt");
+
+		String transtionMapFileName = "../Examples/TestDriver/VCR/TransitionFunctionMap.txt";
+		String testSuiteName = "../Examples/TestDriver/VCR/Breadth/ReplaceThis.txt";
+		System.out.println("Running Test Suite "+ testSuiteName);
+		driver.readFunctionToTransitionMap(transtionMapFileName);
+		boolean success = driver.RunTestCases(testSuiteName); 
+		assertTrue(success);
+	}		
+}
+
