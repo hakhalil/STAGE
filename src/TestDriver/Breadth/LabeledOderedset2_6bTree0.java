c10e976187ee266e6226b615f20de11f24606694
amrsellaithy <amr.ellaithy@gmail.com>
2017-03-29 23:38:23 -0400

    orderedset2_6

---
+++ src/TestDriver/Breadth/LabeledOderedset2_6bTree0.java
@@ -0,0 +1,29 @@
+package TestDriver.OrdSet2_6.Breadth;
+import org.junit.Test;
+
+import TestDriver.OverflowException;
+import TestDriver.TestDriver;
+import junit.framework.TestCase;
+
+public class LabeledOderedset2_6bTree0 extends TestCase{
+
+	int preLast = 0;
+	boolean result = false;
+	int[] preElements = {};
+	int resulting = 0;
+	@Test
+	public void test1() throws OverflowException {
+		
+		TestDriver driver = new TestDriver();
+		 
+//		driver.readStateMap("StateMap.txt");
+
+		String transtionMapFileName = "../Examples/TestDriver/OrdSet2_6/TransitionFunctionMap.txt";
+		String testSuiteName = "../Examples/TestDriver/OrdSet2_6/Breadth/LabeledOderedset2_6bTree0.txt";
+		System.out.println("Running Test Suite "+ testSuiteName);
+		driver.readFunctionToTransitionMap(transtionMapFileName);
+		boolean success = driver.RunTestCases(testSuiteName);
+		assertTrue(success);	
+	}		
+}
+
---
+++ src/TestDriver/RoundTrip/LabeledOderedset2_6rPath.java
@@ -0,0 +1,29 @@
+package TestDriver.OrdSet2_6.RoundTrip;
+import org.junit.Test;
+
+import TestDriver.OverflowException;
+import TestDriver.TestDriver;
+import junit.framework.TestCase;
+
+public class LabeledOderedset2_6rPath extends TestCase{
+
+	int preLast = 0;
+	boolean result = false;
+	int[] preElements = {};
+	int resulting = 0;
+	@Test
+	public void test1() throws OverflowException {
+		
+		TestDriver driver = new TestDriver();
+		 
+//		driver.readStateMap("StateMap.txt");
+
+		String transtionMapFileName = "../Examples/TestDriver/OrdSet2_6/TransitionFunctionMap.txt";
+		String testSuiteName = "../Examples/TestDriver/OrdSet2_6/RoundTrip/LabeledOderedset2_6rPath.txt";
+		System.out.println("Running Test Suite "+ testSuiteName);
+		driver.readFunctionToTransitionMap(transtionMapFileName);
+		boolean success = driver.RunTestCases(testSuiteName);
+		assertTrue(success);	
+	}		
+}
+
